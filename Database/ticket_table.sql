18000898c67b554709139fe102dca90efe7781f9
kshopeyin <71841296+kshopeyin@users.noreply.github.com>
2020-12-10 19:48:33 -0600

    Added Ticket Model and Enum Model and Corected Database names

--- Database/ticket_table.sql
+++ Database/ticket_table.sql
@@ -1,8 +1,18 @@
-﻿CREATE TABLE IF NOT EXISTS `mydb`.`User` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `active_ind` TINYINT NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `password_UNIQUE` (`password` ASC) VISIBLE,
-  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE)
+﻿CREATE TABLE IF NOT EXISTS `mydb`.`Ticket` (
+  `ticket_id` INT NOT NULL AUTO_INCREMENT,
+  `worker_id` INT NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `resolution` VARCHAR(45) NULL,
+  `status_ind` INT NOT NULL,
+  `logger_id` INT NOT NULL,
+  PRIMARY KEY (`ticket_id`),
+  UNIQUE INDEX `ticket_id_UNIQUE` (`ticket_id` ASC) VISIBLE,
+  UNIQUE INDEX `user_id_UNIQUE` (`worker_id` ASC) VISIBLE,
+  INDEX `user_id_idx` (`worker_id` ASC, `logger_id` ASC) VISIBLE,
+  UNIQUE INDEX `logger_id_UNIQUE` (`logger_id` ASC) VISIBLE,
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`worker_id` , `logger_id`)
+    REFERENCES `mydb`.`User` (`user_id` , `user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -2,6 +2,23 @@ namespace BugTracker.Models
 {
     public class Ticket
     {
-        
+        private int TicketId { get; set; }
+        private int WorkerId { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private string Resolution { get; set; }
+        private StatusInd StatusInd { get; set; }
+        private int LoggerId { get; set; }
+
+        public Ticket(int ticketId, int workerId, string title, string description, string resolution, StatusInd statusInd, int loggerId)
+        {
+            TicketId = ticketId;
+            WorkerId = workerId;
+            Title = title;
+            Description = description;
+            Resolution = resolution;
+            StatusInd = statusInd;
+            LoggerId = loggerId;
+        }
     }
 }
