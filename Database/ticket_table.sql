5dd01629528984495601b4f94e5a65abfc738e88
Logan Leavel <Leavel29@outlook.com>
2020-12-10 18:58:18 -0600

    added Ticket and User tables

---
+++ Database/ticket_table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `active_ind` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `password_UNIQUE` (`password` ASC) VISIBLE,
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE)
