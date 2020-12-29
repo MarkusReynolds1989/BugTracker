4281e5388fa4c459ded678f7f7cb72904c3cf56f
Markus <markusreynolds1989@gmail.com>
2020-12-28 20:06:35 -0600

    Worked on middle for Ticket.cshtml

--- Pages/Ticket.cshtml.cs
+++ Pages/Ticket.cshtml.cs
@@ -1,7 +1,7 @@
+using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
-using System.Linq;
 using BugTracker.Controllers;
+using BugTracker.Models;
 
 namespace BugTracker.Pages
 {
@@ -13,6 +13,35 @@ namespace BugTracker.Pages
             ticketController.Init();
             var ticket = ticketController.SelectRow(ticketId);
             ViewData["ticket"] = ticket;
+        }
+        
+        // Updates our current value with the values in the fields.
+        public void OnPost()
+        {
+            // Update everything from page.
+            // TODO: Clean up parsing the int with error handling, or just check with
+            // Typescript. 
+            var ticketId = int.Parse(Request.Form["TicketId"]);
+            var loggerId = int.Parse(Request.Form["LoggerId"]);
+            var workerId = int.Parse(Request.Form["WorkerId"]);
+            var statusIndCd = (StatusIndCd) int.Parse(Request.Form["StatusIndCd"]);
+            var title = Request.Form["Title"];
+            var description = Request.Form["Desc"];
+            var resolution = Request.Form["Resolution"];
+            var ticketController = new TicketController();
+
+            ticketController.Init();
+
+            var updateTicket =
+                new Models.Ticket(ticketId,
+                                  workerId,
+                                  title,
+                                  description,
+                                  resolution,
+                                  statusIndCd,
+                                  loggerId);
+
+            ticketController.Update(updateTicket);
         }
     }
 }
