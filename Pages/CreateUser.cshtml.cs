cb9254be4ef4776cbd6e8c12211738ea40a27b1f
Markus <Markusreynolds1989@protonmail.com>
2021-01-04 18:59:48 -0600

    Tickets.cshtml and more.

---
+++ Pages/CreateUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugTracker.Pages
+{
+    public class CreateUser : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
