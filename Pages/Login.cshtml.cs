ea5e24b1e48e9e3e609ebdd76c90d77e5d75af37
Markus <Markusreynolds1989@protonmail.com>
2021-01-08 19:29:00 -0600

    Implemented basic authentication in Login.cshtml.cs

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using BugTracker.Controllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BugTracker.Pages
@@ -10,9 +12,16 @@ namespace BugTracker.Pages
             var userName = ViewData["UserName"].ToString();
             var password = ViewData["Password"].ToString();
             var login = new LoginController(userName, password);
-            if (login != null)
+            if (login.AuthorizeUser() != null)
             {
-                
+                var userId = login.AuthorizeUser().UserId;
+                // Set the users session user id here.
+                HttpContext.Session.SetInt32("UserId", userId);
+                Response.Redirect("Tickets/");
+            }
+            else
+            {
+                // No session info, tell the user they can't log in.
             }
         }
     }
