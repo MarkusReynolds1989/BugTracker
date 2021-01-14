5c947333686cb8ccc2e9499c0a781d7a43e64742
Markus <Markusreynolds1989@protonmail.com>
2021-01-13 18:45:56 -0600

    Made login page default page.

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace BugTracker
             // Added for sessions.
             services.AddSession();
             services.AddMemoryCache();
+            services.AddMvc().AddRazorPagesOptions(options => { options.Conventions.AddPageRoute("/Login", ""); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
