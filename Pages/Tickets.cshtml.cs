7a20acfab71091b7528c27e7c099e4b44eecfd42
Markus <Markusreynolds1989@protonmail.com>
2021-01-13 19:08:34 -0600

    Navbar, icons.

--- Pages/Tickets.cshtml.cs
+++ Pages/Tickets.cshtml.cs
@@ -15,16 +15,18 @@ namespace BugTracker.Pages
             {
                 Response.Redirect("Login");
             }
-            
-            // Create a new ticket controller object on the heap.
-            var ticketController = new TicketController();
-            // Initialize the database connection and string.
-            ticketController.Init();
-            // Gather all the tickets assigned to the particular userId.
-            // Have to use this BitConverter here to get the data.
-            var tickets = ticketController.SelectAll(userId);
-            // You can use this on the page now.
-            ViewData["Tickets"] = tickets;
+            else
+            {
+                // Create a new ticket controller object on the heap.
+                var ticketController = new TicketController();
+                // Initialize the database connection and string.
+                ticketController.Init();
+                // Gather all the tickets assigned to the particular userId.
+                // Have to use this BitConverter here to get the data.
+                var tickets = ticketController.SelectAll(userId);
+                // You can use this on the page now.
+                ViewData["Tickets"] = tickets;
+            }
         }
     }
 }
