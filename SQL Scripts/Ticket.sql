32db0ff97f09396165cb20ceb494cd9a7592a4f3
Logan Leavel <Leavel29@outlook.com>
2021-01-25 18:42:23 -0600

    added SQL scripts

---
+++ SQL Scripts/Ticket.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `Ticket` (
+  `ticket_id` int(11) NOT NULL AUTO_INCREMENT,
+  `worker_id` int(11) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `resolution` varchar(300) DEFAULT NULL,
+  `status_ind` int(11) NOT NULL,
+  `logger_id` int(11) NOT NULL,
+  `active_ind` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`ticket_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4
---
+++ SQL Scripts/User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `User` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `active_ind` tinyint(4) NOT NULL DEFAULT 1,
+  `auth_level` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4
