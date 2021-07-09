5aeb32ac74219ac693e577aac55e898f0c445df1
Markus <Markusreynolds1989@protonmail.com>
2021-07-09 06:14:21 -0500

    Switched classes to immutable init only record types

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,40 +1,5 @@
 namespace BugTracker.Models
 {
-    public class Ticket
-    {
-        public int TicketId { get; }
-        public int WorkerId { get; }
-        public string Title { get; }
-
-        public string Description { get; }
-
-        public string Resolution { get; }
-        public StatusIndCd StatusIndCd { get; }
-        public int LoggerId { get; }
-
-        // Constructor for ticket that already exists.
-        public Ticket(int ticketId, int workerId, string title, string description, string resolution,
-            StatusIndCd statusIndCd, int loggerId)
-        {
-            TicketId = ticketId;
-            WorkerId = workerId;
-            Title = title;
-            Description = description;
-            Resolution = resolution;
-            StatusIndCd = statusIndCd;
-            LoggerId = loggerId;
-        }
-
-        // Constructor for creating a new ticket.
-        public Ticket(int workerId, string title, string description, string resolution,
-            StatusIndCd statusIndCd, int loggerId)
-        {
-            WorkerId = workerId;
-            Title = title;
-            Description = description;
-            Resolution = resolution;
-            StatusIndCd = statusIndCd;
-            LoggerId = loggerId;
-        }
-    }
+    public record Ticket(int WorkerId, string Title, string Description, string Resolution, int LoggerId,
+        StatusIndCd StatusIndCd = StatusIndCd.Open, int? TicketId = null);
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,43 +1,8 @@
-using System.Runtime.CompilerServices;
-
 namespace BugTracker.Models
 {
-    public class User
-    {
-        public int UserId { get; }
-        public string UserName { get; }
-        public string Firstname { get; }
-        public string Lastname { get; }
-        public string Password { get; }
-        public string Email { get; }
-        public bool ActiveInd { get; }
-        public AuthLevel AuthLevel { get; }
-
-        // This constructor is for a user that already exists.
-        public User(int userId, string userName, string firstname, string lastname, string password, string email,
-            bool activeInd, AuthLevel authLevel)
-        {
-            UserId = userId;
-            UserName = userName;
-            Firstname = firstname;
-            Lastname = lastname;
-            Password = password;
-            Email = email;
-            ActiveInd = activeInd;
-            AuthLevel = authLevel;
-        }
-
-        // This constructor is for creating a new user.
-        public User(string userName, string firstName, string lastname, string password, string email,
-            AuthLevel authLevel)
-        {
-            UserName = userName;
-            Firstname = firstName;
-            Lastname = lastname;
-            Password = password;
-            Email = email;
-            ActiveInd = true;
-            AuthLevel = authLevel;
-        }
-    }
+    // UserId is nullable in the situation where we want to create a new user.
+    // Also default auth level to guest, it is defaulted on the sql side as well.
+    // Also default active ind, it is defaulted on the sql side as well.
+    public record User (string UserName, string FirstName, string LastName, string Password, string Email,
+        bool ActiveInd = true, AuthLevel AuthLevel = AuthLevel.Guest, int? UserId = null);
 }
