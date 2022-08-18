72c29f68aa2de9ea870a6b5fed2f92687bb032d0
Markus Reynolds <markusreynolds1989@gmail.com>
2022-08-18 14:39:43 -0500

    Removed markdown, fixed test

---
+++ src/BugTrackerTest/ControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BugTrackerTest
+{
+    public class ControllerTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
