5808d43cfefc78f3e283a498af21984dab56cb25
Markus Reynolds <markusreynolds1989@gmail.com>
2022-08-21 06:20:15 -0500

    Build script changes

--- build.fsx
+++ build.fsx
@@ -1,23 +1,38 @@
-open System
 open System.Diagnostics
 
 // Download the mysql database on Windows, for test env or local use.
+// Must run build from an elevated shell (admin).
 let sqlInstallProcessInfo =
     ProcessStartInfo(
         FileName = "powershell.exe",
-        Arguments = "choco install mysql",
+        Arguments = "choco -y install mysql",
         UseShellExecute = false,
         RedirectStandardOutput = true,
-        RedirectStandardInput = true,
         Verb = "runas",
         RedirectStandardError = true
     )
 
+let sqlStartProcessInfo =
+    ProcessStartInfo(FileName = "mysql", UseShellExecute = false, Verb = "runas")
+
+printfn "Starting MySql Server Check..."
+
 let sqlInstallProcess =
     Process.Start(sqlInstallProcessInfo)
-printf "Starting MySql Server Check..."
-sqlInstallProcess.StandardInput.WriteLine("a")
-sqlInstallProcess.WaitForExit()
-printf "Finished MySql Server..."
 
+sqlInstallProcess.WaitForExit()
+
+printfn "Finished MySql Server Check..."
+
+printfn "Starting MySql Server on Port:"
+Process.Start(sqlStartProcessInfo)
+printfn "Setting up default scheme..."
+printfn "Setting up tables..."
+printfn "MySql DB now ready for testing."
+
+printfn "Building solution..."
+Process.Start("dotnet build")
+
+printfn "Solution built."
 // Download the mysql database on Ubuntu, for production servers.
+// Must run from an elevated shell, sudo.
