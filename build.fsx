c2d9ac8e6cad1ab0f1df6e845e7f171bd9b3cf4f
Markus Reynolds <markusreynolds1989@gmail.com>
2022-08-30 06:50:52 -0500

    Build script can now download mysql server

--- build.fsx
+++ build.fsx
@@ -1,38 +1,66 @@
+open System
+open System.IO
 open System.Diagnostics
+
+let mutable mysqlInstalled = false
+let sqlCountProcessInfo =
+    ProcessStartInfo(
+        FileName = "powershell.exe",
+        Arguments = "(ps | findstr mysql).count",
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        Verb = "runas")
+    
+let mysqlProcessCount = Process.Start(sqlCountProcessInfo)
+mysqlProcessCount.WaitForExit()
+
+let count = int (mysqlProcessCount.StandardOutput.ReadToEnd())
+if count > 0 then mysqlInstalled <- true
 
 // Download the mysql database on Windows, for test env or local use.
 // Must run build from an elevated shell (admin).
-let sqlInstallProcessInfo =
+let sqlDownloadProcessInfo =
     ProcessStartInfo(
         FileName = "powershell.exe",
-        Arguments = "choco -y install mysql",
+        Arguments = "invoke-webrequest -URI https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-community-8.0.30.0.msi -OutFile ./mysql.msi",
         UseShellExecute = false,
         RedirectStandardOutput = true,
         Verb = "runas",
         RedirectStandardError = true
     )
-
-let sqlStartProcessInfo =
-    ProcessStartInfo(FileName = "mysql", UseShellExecute = false, Verb = "runas")
-
+    
+let sqlInstallProcessInfo =
+    ProcessStartInfo(
+        FileName = "powershell.exe",
+        Arguments = "msiexec.exe /I mysql.msi",
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        Verb = "runas",
+        RedirectStandardError = true)
+    
 printfn "Starting MySql Server Check..."
 
-let sqlInstallProcess =
-    Process.Start(sqlInstallProcessInfo)
 
-sqlInstallProcess.WaitForExit()
+if not mysqlInstalled then
+    printfn "MySql Not found, downloading as mysql.msi...\n\n\n\n"
+    let sqlDownloadProcess =
+        Process.Start(sqlDownloadProcessInfo)
 
+    sqlDownloadProcess.WaitForExit()
+
+    printfn "Mysql Downloaded."
+
+    printfn "Installing mysql ..."
+    let sqlInstallProcess =
+        Process.Start(sqlInstallProcessInfo)
+
+    sqlInstallProcess.WaitForExit()
+    
+    File.Delete("mysql.msi")
+    
 printfn "Finished MySql Server Check..."
 
-printfn "Starting MySql Server on Port:"
-Process.Start(sqlStartProcessInfo)
-printfn "Setting up default scheme..."
-printfn "Setting up tables..."
-printfn "MySql DB now ready for testing."
-
 printfn "Building solution..."
-Process.Start("dotnet build")
+Process.Start("powershell.exe", "cd src; dotnet build")
 
 printfn "Solution built."
-// Download the mysql database on Ubuntu, for production servers.
-// Must run from an elevated shell, sudo.
